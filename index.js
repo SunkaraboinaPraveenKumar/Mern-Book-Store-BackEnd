46778b5258973111ccc6fe76573154355c7cf49e
SunkaraboinaPraveenKumar <praveenkumargitsunkaraboina@gmail.com>
2024-05-22 10:17:12 +0530

    link changes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 // Middle ware
 app.use(cors({
-  origin: 'https://mern-book-store-fronend.vercel.app',
+  origin: ['https://mern-book-store-fronend.vercel.app','http://localhost:5000/all-books','http://localhost:5000/book/:id'],
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
